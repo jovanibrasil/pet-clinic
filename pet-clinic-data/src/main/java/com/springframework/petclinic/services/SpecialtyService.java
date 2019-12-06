f4cd548a5f8d72837a25692d1caeb96ddbf01d3e
Jovani Oliveira Brasil <jovanibrasil@gmail.com>
2019-12-06 00:04:39 -0300

    Add Specialty Map impl. Closes #30

---
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/services/SpecialtyService.java
@@ -0,0 +1,5 @@
+package com.springframework.petclinic.services;
+
+import com.springframework.petclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> { }
