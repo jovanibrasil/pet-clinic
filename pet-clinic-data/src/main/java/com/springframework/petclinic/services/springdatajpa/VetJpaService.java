9f33d1eaf60696de1e5b436eb1ad54f4c0472d06
Jovani Oliveira Brasil <jovanibrasil@gmail.com>
2019-12-08 14:00:27 -0300

    Add vet jpa service implementation. Closes #40

---
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/services/springdatajpa/VetJpaService.java
@@ -0,0 +1,17 @@
+package com.springframework.petclinic.services.springdatajpa;
+
+import com.springframework.petclinic.model.Vet;
+import com.springframework.petclinic.repositories.VetRepository;
+import com.springframework.petclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Profile("jpa")
+@Service
+public class VetJpaService extends AbstractJpaService<Vet, Long, VetRepository> implements VetService {
+
+    public VetJpaService(VetRepository repository) {
+        super(repository);
+    }
+
+}
