638f24f16a691c6fb8e873aebe5bb449b6c3ce3c
Jovani Oliveira Brasil <jovanibrasil@gmail.com>
2019-12-08 14:30:15 -0300

    Add specialties jpa service implementation

---
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/services/springdatajpa/SpecialtyJpaService.java
@@ -0,0 +1,15 @@
+package com.springframework.petclinic.services.springdatajpa;
+
+import com.springframework.petclinic.model.Specialty;
+import com.springframework.petclinic.repositories.SpecialtyRepository;
+import com.springframework.petclinic.services.SpecialtyService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Profile("jpa")
+@Service
+public class SpecialtyJpaService extends AbstractJpaService<Specialty, Long, SpecialtyRepository> implements SpecialtyService {
+    public SpecialtyJpaService(SpecialtyRepository repository) {
+        super(repository);
+    }
+}
